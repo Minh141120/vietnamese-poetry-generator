f9eff36071ae523dd62a2a0a1e5d629492835318
Nguyen Ngoc Minh <91668012+Minh141120@users.noreply.github.com>
2024-12-20 16:52:15 +0700

    Create text_utils.py

---
+++ utils/text_utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+import tensorflow as tf
+
+def sample_with_temperature(preds, temperature=1.0):
+    """Sample an index from a probability array using temperature."""
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
